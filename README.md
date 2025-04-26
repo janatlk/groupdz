66cc2b55f7ae6f8da042109f7621fef244684e56
MUS7ANG <salihjudo@icloud.com>
2025-04-26 14:52:27 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Group Project</h1>
+<p>fork it</p>
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Card, Input} from "@mui/material"
+import './App.css'
+function App() {
+
+  return (
+    <Card>
+      <Input value={"Elkhan loh pozorni"}></Input>
+    </Card>
+  )
+}
+
+export default App
