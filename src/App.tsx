1df3d5f56939419b74fc2ab8e5468f40f7720e34
MUS7ANG <salihjudo@icloud.com>
2025-04-28 17:14:24 +0600

    project update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import {Card, Input} from "@mui/material"
-import './App.css'
+import {CssBaseline} from '@mui/material';
+import {Routes, Route} from 'react-router';
+import {Login} from './pages/Login.tsx';
+import {Register} from './pages/Register.tsx';
+import {Home} from './pages/Home.tsx';
+import {CreatePost} from './pages/CreatePost.tsx';
+import Header from './components/Header.tsx';
+
 function App() {
 
   return (
-    <Card>
-      <Input value={"Elkhan loh pozorni"}></Input>
-    </Card>
-  )
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={<Home/>}/>
+        <Route path={'/login'} element={<Login/>}/>
+        <Route path={'/register'} element={<Register/>}/>
+        <Route path={'/create-post'} element={<CreatePost/>}/>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
